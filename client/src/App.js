609bf48ae2707a3a003de4a241ed3efd68a4491f
Mika6413 <https://github.com/Mika6413>
2024-06-03 15:16:08 +0200

    ajout d'un <h2>

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to CI</h1>
+        <h2>and Github actions...</h2>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
